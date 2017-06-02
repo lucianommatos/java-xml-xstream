deb17e128620685dc9b87b3d6854c173ba53bf56
Luciano Matos <lucianommatos@gmail.com>
2017-06-01 23:26:44 -0300

    Removing unused Exception

--- src/br/com/xstream/main/MainXStreamTest.java
+++ src/br/com/xstream/main/MainXStreamTest.java
@@ -1,14 +1,12 @@
 package br.com.xstream.main;
 
-import java.io.FileNotFoundException;
-
 import br.com.xstream.domain.Product;
 import br.com.xstream.domain.ShoppingCart;
 import br.com.xstream.parser.XStreamParser;
 
 public class MainXStreamTest {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		ShoppingCart shoppingCart = new ShoppingCart();
 		shoppingCart.getProductList().add(new Product("Notebook", "2000.0"));
 		shoppingCart.getProductList().add(new Product("Joana D'Arc Book", "27.0"));
